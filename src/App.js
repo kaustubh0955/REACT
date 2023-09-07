f456119bfcbad59b66d38a9b476a4e40dcc3993c
kaustubh0955 <kaustubhrai0955@gmail.com>
2023-09-07 20:23:45 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -71,6 +71,11 @@ function TabContent({ item }) {
     setLikes(likes + 1);
   }
 
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+  }
+
   return (
     <div className="tab-content">
       <h4>{item.summary}</h4>
@@ -89,7 +94,7 @@ function TabContent({ item }) {
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
+        <button onClick={handleUndo}>Undo</button>
         <button>Undo in 2s</button>
       </div>
     </div>
