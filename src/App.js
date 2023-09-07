0514e2df86bd54070020986b251f7ba8d8729c78
kaustubh0955 <kaustubhrai0955@gmail.com>
2023-09-07 12:02:30 +0530

    day2

--- src/App.js
+++ src/App.js
@@ -26,6 +26,8 @@ export default function App() {
   );
 }
 
+console.log(<DifferentContent test={23} />);
+
 function Tabbed({ content }) {
   const [activeTab, setActiveTab] = useState(0);
 
@@ -39,7 +41,10 @@ function Tabbed({ content }) {
       </div>
 
       {activeTab <= 2 ? (
-        <TabContent item={content.at(activeTab)} />
+        <TabContent
+          item={content.at(activeTab)}
+          key={content.at(activeTab.summary)}
+        />
       ) : (
         <DifferentContent />
       )}
